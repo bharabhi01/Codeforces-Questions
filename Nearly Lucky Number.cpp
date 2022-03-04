d00fb86ef99c7eb7b232e8d09402c4bc713a76ea
Abhishek Bhardwaj <93217667+bharabhi01@users.noreply.github.com>
2022-03-04 20:39:28 +0530

    Add files via upload

---
+++ Nearly Lucky Number.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main() {
+    long long num;
+    cin>>num;
+
+    int count = 0;
+
+    while(num != 0)
+    {
+        if(num % 10 == 4 || num % 10 == 7)
+            count++;
+
+        num /= 10;
+    }
+
+    if(count == 4 || count == 7)
+        cout<<"YES";
+    else 
+        cout<<"NO";
+
+    return 0;
+}
